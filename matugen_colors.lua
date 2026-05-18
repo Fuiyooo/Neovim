c5f3c92995030c3b517fedcbd5bc9b108d47c16f
Fuiyooo <cliffpermadi08@gmail.com>
2026-05-19 02:54:12 +0700

    Add more plugins

---
+++ matugen_colors.lua
@@ -0,0 +1,35 @@
+return {
+  -- Core Backgrounds (Deepest Darks)
+  crust     = "#0b0f12",
+  mantle    = "#181c20",
+  base      = "#101418",
+  
+  -- UI Elements & Overlays
+  surface0  = "#1c2024",
+  surface1  = "#272a2e",
+  surface2  = "#313539",
+  overlay0  = "#42474e",
+  overlay1  = "#8c9198",
+  overlay2  = "#c2c7ce",
+  
+  -- Text & Foreground (Brightest Light)
+  subtext0  = "#c2c7ce",
+  subtext1  = "#e0e2e8",
+  text      = "#e0e2e8",
+  
+  -- Syntax Highlights (Bright Accents ONLY)
+  rosewater = "#99ccfa",
+  flamingo  = "#d2bfe7",
+  pink      = "#d2bfe7",
+  mauve     = "#d2bfe7",
+  red       = "#ffb4ab",
+  maroon    = "#ffb4ab",
+  peach     = "#b8c8da",
+  yellow    = "#b8c8da",
+  green     = "#99ccfa",
+  teal      = "#b8c8da",
+  sky       = "#99ccfa",
+  sapphire  = "#b8c8da",
+  blue      = "#99ccfa",
+  lavender  = "#d2bfe7",
+}
